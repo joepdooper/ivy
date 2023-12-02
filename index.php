a38f0a787a01a0440adbfaa3b2ea206f8e977845
Joep <joepdooper@hotmail.com>
2023-12-02 17:28:46 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-error_reporting(E_ALL);
-ini_set('ignore_repeated_errors', TRUE);
-ini_set('display_errors', TRUE);
-ini_set('log_errors', TRUE);
-ini_set('error_log', 'logs/php_error.txt');
+// error_reporting(E_ALL);
+// ini_set('ignore_repeated_errors', TRUE);
+// ini_set('display_errors', TRUE);
+// ini_set('log_errors', TRUE);
+// ini_set('error_log', 'logs/php_error.txt');
 
 // Init session
 session_start();
