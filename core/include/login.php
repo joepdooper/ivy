3ce84a716dbe69cb2c50b37d70ae276ef00d0596
Joep <joepdooper@hotmail.com>
2023-12-04 00:22:53 +0100

    Update login.php

--- core/include/login.php
+++ core/include/login.php
@@ -4,27 +4,32 @@ defined('_BASE_PATH') ?: header('location: ../../index.php');
 // Confirm Email
 if (isset($selector) && isset($token)){
   try {
-      $auth->confirmEmail($selector, $token);
-      if ($auth->isLoggedIn()) {
-        $auth->logOut();
+    $auth->confirmEmail($selector, $token);
+    if ($auth->isLoggedIn()) {
+      try {
+        $auth->logOutEverywhere();
       }
-      \Ivy\Message::add('Email address has been verified',_BASE_PATH . 'admin/login');
+      catch (\Delight\Auth\NotLoggedInException $e) {
+        \Ivy\Message::add('Not logged in');
+      }
+    }
+    \Ivy\Message::add('Email address has been verified',_BASE_PATH . 'admin/login');
   }
   catch (\Delight\Auth\InvalidSelectorTokenPairException $e) {
     \Ivy\Message::add('Invalid token');
-      // die('Invalid token');
+    // die('Invalid token');
   }
   catch (\Delight\Auth\TokenExpiredException $e) {
     \Ivy\Message::add('Token expired');
-      // die('Token expired');
+    // die('Token expired');
   }
   catch (\Delight\Auth\UserAlreadyExistsException $e) {
     \Ivy\Message::add('Email address already exists');
-      // die('Email address already exists');
+    // die('Email address already exists');
   }
   catch (\Delight\Auth\TooManyRequestsException $e) {
     \Ivy\Message::add('Too many requests');
-      // die('Too many requests');
+    // die('Too many requests');
   }
 }
 ?>
