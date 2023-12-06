be4ae7182456ec0579fc13c50018e277338bfa08
Joep <joepdooper@hotmail.com>
2023-12-06 23:23:18 +0100

    Removed Vimeo and Youtube plugin

--- plugins/vimeo/classes/class.Vimeo.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Vimeo extends Ivy\Model {
-
-  public $id, $vimeo_video_id, $token;
-  protected $table = "vimeo";
-
-}
-?>
--- plugins/youtube/classes/class.Youtube.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Youtube extends Ivy\Model {
-
-  public $id, $youtube_video_id, $token;
-  protected $table = "youtube";
-
-}
-?>
