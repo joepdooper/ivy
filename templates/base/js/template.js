a621e7271387734551024cd9f2dd6c2289a9e4c8
Joep <joepdooper@hotmail.com>
2024-01-20 22:46:17 +0100

    Update template.js

--- templates/base/js/template.js
+++ templates/base/js/template.js
@@ -1,16 +1,16 @@
 import * as axios from '../../../node_modules/axios/dist/axios.min.js';
 import { createApp, reactive } from '../../../node_modules/petite-vue/dist/petite-vue.es.js';
-import * as swup from '../../../node_modules/swup/dist/Swup.umd.js';
-
-const swp = new Swup;
-
-swp.hooks.on('page:view', () => {
-  call_my_function("swupPageView");
-});
-
-swp.hooks.before('content:replace', () => {
-	call_my_function("swupContentReplace");
-});
+// import * as swup from '../../../node_modules/swup/dist/Swup.umd.js';
+//
+// const swp = new Swup;
+//
+// swp.hooks.on('page:view', () => {
+//   call_my_function("swupPageView");
+// });
+//
+// swp.hooks.before('content:replace', () => {
+// 	call_my_function("swupContentReplace");
+// });
 
 window.addEventListener('DOMContentLoaded', (event) => {
 	document.querySelectorAll('[type=submit]').forEach(function(el){
