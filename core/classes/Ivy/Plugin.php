2357e0a59d70bf6cf66ca9d2b053eac40fd23aec
Joep <joepdooper@hotmail.com>
2024-03-08 22:58:21 +0100

    Update Plugin.php

--- core/classes/Ivy/Plugin.php
+++ core/classes/Ivy/Plugin.php
@@ -69,30 +69,32 @@ class Plugin extends Model {
             // -- install plugin
             if(!isset($plugin['id'])) {
                 try {
+                    if (strpos(_PUBLIC_PATH . _PLUGIN_PATH . $plugin['id'], _PUBLIC_PATH . _PLUGIN_PATH) === 0) {
 
-                    $this->plugin = $this->loadPluginInfo($plugin);
+                        $this->plugin = $this->loadPluginInfo($plugin);
 
-                    if (isset($this->plugin->dependencies) && !empty($missing = $this->checkDependencies((array)$this->plugin->dependencies))) {
-                        $count = count($missing);
-                        $message = "This plugin has " . ($count > 1 ? "dependencies" : "dependency") . ". Please install the " . ($count > 1 ? "plugins" : "plugin") . " " . implode(", ", $missing);
-                        Message::add($message, _BASE_PATH . 'admin/plugin');
-                    }
-
-                    $_POST['plugin'][$key] = [
-                        'name' => $purifier->purify($this->plugin->name),
-                        'version' => $purifier->purify($this->plugin->version),
-                        'desc' => $purifier->purify($this->plugin->description),
-                        'url' => $purifier->purify($this->plugin->url),
-                        'type' => $purifier->purify($this->plugin->type),
-                        'settings' => (!empty($this->plugin->settings) ? '1' : '0')
-                    ];
-
-                    if(!empty($this->plugin->database->install)) {
-                        if (file_exists(_PUBLIC_PATH . _PLUGIN_PATH . $this->plugin->url . DIRECTORY_SEPARATOR . $this->plugin->database->install)) {
-                            require_once _PUBLIC_PATH . _PLUGIN_PATH . $this->plugin->url . DIRECTORY_SEPARATOR . $this->plugin->database->install;
+                        if (isset($this->plugin->dependencies) && !empty($missing = $this->checkDependencies((array)$this->plugin->dependencies))) {
+                            $count = count($missing);
+                            $message = "This plugin has " . ($count > 1 ? "dependencies" : "dependency") . ". Please install the " . ($count > 1 ? "plugins" : "plugin") . " " . implode(", ", $missing);
+                            Message::add($message, _BASE_PATH . 'admin/plugin');
                         }
-                    }
 
+                        $_POST['plugin'][$key] = [
+                            'name' => $purifier->purify($this->plugin->name),
+                            'version' => $purifier->purify($this->plugin->version),
+                            'desc' => $purifier->purify($this->plugin->description),
+                            'url' => $purifier->purify($this->plugin->url),
+                            'type' => $purifier->purify($this->plugin->type),
+                            'settings' => (!empty($this->plugin->settings) ? '1' : '0')
+                        ];
+
+                        if(!empty($this->plugin->database->install)) {
+                            if (file_exists(_PUBLIC_PATH . _PLUGIN_PATH . $this->plugin->url . DIRECTORY_SEPARATOR . $this->plugin->database->install)) {
+                                require_once _PUBLIC_PATH . _PLUGIN_PATH . $this->plugin->url . DIRECTORY_SEPARATOR . $this->plugin->database->install;
+                            }
+                        }
+
+                    }
                 } catch (Exception $e) {
                     Message::add("Error installing plugin: " . $e->getMessage());
                 }
@@ -140,7 +142,10 @@ class Plugin extends Model {
 
     private function loadPluginInfo($plugin)
     {
-        $dir = _PUBLIC_PATH . _PLUGIN_PATH . $plugin['url'] . DIRECTORY_SEPARATOR;
+        $config = HTMLPurifier_Config::createDefault();
+        $purifier = new HTMLPurifier($config);
+
+        $dir = _PUBLIC_PATH . _PLUGIN_PATH . $purifier->purify($plugin['url']) . DIRECTORY_SEPARATOR;
 
         if (strpos($dir, _PUBLIC_PATH . _PLUGIN_PATH) === 0) {
 
