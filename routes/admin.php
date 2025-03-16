48797ccffb92e0b53ab4980efb31fff58297c6ac
Joep <joepdooper@hotmail.com>
2025-03-16 11:45:43 +0100

    Update routes

--- routes/admin.php
+++ routes/admin.php
@@ -1,61 +1,33 @@
 <?php
 
 use Ivy\App;
-use Ivy\Path;
-use Ivy\User;
 
 App::router()->mount('/admin', function () {
-
+    // -- USER register
     App::router()->post('/user/register', '\Ivy\UserController@register');
-
+    App::router()->get('/register', '\Ivy\UserController@viewRegister');
+    // -- USER login
     App::router()->post('/user/login', '\Ivy\UserController@login');
-
+    App::router()->get('/login(/[^/]+)?(/[^/]+)?', '\Ivy\UserController@viewLogin');
+    // -- USER logout
     App::router()->post('/user/logout', '\Ivy\UserController@logout');
-
+    App::router()->get('/logout', '\Ivy\UserController@viewLogout');
+    // -- USER reset
     App::router()->post('/user/reset', '\Ivy\UserController@reset');
-
-    App::router()->post('/user/post', '\Ivy\UserController@post');
-
-    App::router()->post('/plugin/post', '\Ivy\PluginController@post');
-
+    App::router()->get('/reset(/[^/]+)?(/[^/]+)?', '\Ivy\UserController@viewReset');
+    // -- USER profile
     App::router()->post('/profile/post', '\Ivy\ProfileController@post');
-
-    App::router()->post('/setting/post', '\Ivy\SettingController@post');
-
+    App::router()->get('/profile', '\Ivy\ProfileController@user');
+    // -- USER index
+    App::router()->post('/user/post', '\Ivy\UserController@post');
+    App::router()->get('/user', '\Ivy\UserController@index');
+    // -- PLUGIN index
+    App::router()->post('/plugin/post', '\Ivy\PluginController@post');
+    App::router()->get('/plugin(/[a-z0-9_-]+)?(/collection)?', '\Ivy\PluginController@index');
+    // -- TEMPLATE index
     App::router()->post('/template/post', '\Ivy\TemplateController@post');
-
-    App::router()->get('/(\w+)(/[^/]+)?(/[^/]+)?', function ($id, $selector = null, $token = null) {
-        switch ($id) {
-            case "reset":
-                if (User::getAuth()->isLoggedIn()) {
-                    header('location: ' . Path::get('BASE_PATH') . 'admin/profile');
-                    exit;
-                }
-                include Path::get('PUBLIC_PATH') . 'include/reset.php';
-                break;
-            case "login":
-                if (User::getAuth()->isLoggedIn()) {
-                    header('location: ' . Path::get('BASE_PATH') . 'admin/profile');
-                    exit;
-                }
-                include Path::get('PUBLIC_PATH') . 'include/login.php';
-                break;
-            case "profile":
-                if (User::getAuth()->isLoggedIn() && $selector && $token) {
-                    include Path::get('PUBLIC_PATH') . 'include/login.php';
-                }
-                break;
-        }
-        if (!User::getAuth()->isLoggedIn() && !in_array($id, ['login', 'reset', 'register'])) {
-            header('location:' . Path::get('BASE_PATH') . 'admin/login');
-            exit;
-        }
-        if (User::getAuth()->isLoggedIn() && in_array($id, ['setting', 'plugin', 'register', 'reset', 'template', 'user'])) {
-            if (!User::canEditAsAdmin()) {
-                header('location:' . Path::get('BASE_PATH') . 'admin/profile');
-                exit;
-            }
-        }
-    });
-
+    App::router()->get('/template', '\Ivy\TemplateController@index');
+    // -- SETTING index
+    App::router()->post('/setting/post', '\Ivy\SettingController@post');
+    App::router()->get('/setting', '\Ivy\SettingController@index');;
 });
