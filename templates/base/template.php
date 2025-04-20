71fbdb48b5a6c49483551e4f3f99f2229b98d924
Joep <joepdooper@hotmail.com>
2025-04-20 20:15:36 +0200

    Update template

--- templates/base/template.php
+++ templates/base/template.php
@@ -7,4 +7,5 @@ AssetManager::addCSS('css/custom.css');
 
 // AssetManager::addJS('node_modules/axios/dist/axios.min.js');
 // AssetManager::addJS('node_modules/petite-vue/dist/petite-vue.umd.js');
-// AssetManager::addJS('templates/base/js/template.js');
+// AssetManager::addESM('templates/base/js/template.js');
+// AssetManager::addESM('node_modules/callbackhooks/callbackhooks.js');
