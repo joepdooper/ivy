9571aa1d9d3c3ddaa9bad574d5448697a815880c
Joep <joepdooper@hotmail.com>
2025-08-03 13:54:55 +0200

    Update autoloader.php

--- autoloader.php
+++ autoloader.php
@@ -1,12 +1,12 @@
 <?php
 
-use Ivy\Path;
+use Ivy\Core\Path;
 
 spl_autoload_register(function ($classname) {
     $classname = str_replace('\\', DIRECTORY_SEPARATOR, $classname);
     $namespace = dirname(str_replace(['\\', '/'], DIRECTORY_SEPARATOR, $classname));
 
-    $directory = Path::get('PUBLIC_PATH') . 'plugins' . DIRECTORY_SEPARATOR . strtolower($namespace) . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR;
+    $directory = Path::get('PROJECT_PATH') . 'plugins' . DIRECTORY_SEPARATOR . strtolower($namespace) . DIRECTORY_SEPARATOR . 'classes' . DIRECTORY_SEPARATOR;
 
     $filePath = $directory . basename($classname) . '.php';
     if (file_exists($filePath)) {
