9843fa96f43921eb08ae31aea9432759aea659e7
Joep <joepdooper@hotmail.com>
2025-08-24 22:00:35 +0200

    Fix templates

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Ivy\Manager\RouterManager;
 
+// -- HOME
+RouterManager::instance()->get('/', '\Ivy\Controller\TemplateController@root');
 // -- PROFILE
 RouterManager::instance()->get('/profile/(\d+)', '\Ivy\Controller\ProfileController@public');
