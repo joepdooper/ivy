7da847c541fb4ce74e343cc6902121ccab46b817
Joep <joepdooper@hotmail.com>
2025-10-12 01:23:11 +0200

    Remove timeline template

--- templates/timeline/plugins/items/language/en/modal.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'insert_item' => 'Insert item',
-    'insert_selected_item' => 'Insert selected item',
-    'create_new_moment' => 'Create a new moment',
-    'add_new_moment' => 'Add a new moment',
-];
