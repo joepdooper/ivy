c941d417077531cdcdeac4139e978fd33cb9118b
Joep <joepdooper@hotmail.com>
2025-10-26 17:28:21 +0100

    Update ivy.sql

--- ivy.sql
+++ ivy.sql
@@ -113,7 +113,14 @@ VALUES
 	(2,'Minify CSS',0,'','CSS minifier',NULL,NULL,1),
 	(3,'Minify JS',0,'','JS minifier',NULL,NULL,1),
 	(4,'Registration role',0,'EDITOR','After registration set user role',NULL,NULL,1),
-	(44,'Test',0,'EDITOR','',NULL,NULL,0);
+	(11,'Sort',0,NULL,'Sort items by drag and drop',NULL,37,0),
+	(12,'Timeline',0,NULL,'Show items in a timeline',NULL,37,0),
+	(13,'Sort',0,NULL,'Sort items by drag and drop',NULL,38,0),
+	(14,'Timeline',0,NULL,'Show items in a timeline',NULL,38,0),
+	(17,'Sort',0,NULL,'Sort items by drag and drop',NULL,49,0),
+	(18,'Timeline',0,NULL,'Show items in a timeline',NULL,49,0),
+	(19,'Sort',0,NULL,'Sort items by drag and drop',NULL,50,0),
+	(20,'Timeline',0,NULL,'Show items in a timeline',NULL,50,0);
 
 /*!40000 ALTER TABLE `settings` ENABLE KEYS */;
 UNLOCK TABLES;
