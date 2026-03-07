ef0e2f2d683bc2305595732e7ad434bdb66221bb
joep <joepdooper@hotmail.com>
2026-03-07 21:37:08 +0100

    Tailwind for vite

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig, loadEnv } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig(({ mode }) => {
+
     const env = loadEnv(mode, process.cwd(), '')
     const isDev = env.APP_ENV === 'development'
 
     return {
+        plugins: [
+            tailwindcss(),
+        ],
+
         base: isDev ? '/' : '/js/',
 
         build: {
