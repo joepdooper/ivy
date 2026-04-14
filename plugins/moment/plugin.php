f8df9fe06d31d83b0d3fea54c0c73074647e4c6c
Joep <joepdooper@hotmail.com>
2026-04-15 00:03:35 +0200

    Update plugin routes

--- plugins/moment/plugin.php
+++ plugins/moment/plugin.php
@@ -1,35 +1,37 @@
 <?php
 
 use Items\ItemRegistry;
-use Ivy\Manager\AssetManager;
-use Ivy\Manager\RouterManager;
-use Ivy\Model\User;
+use Ivy\Routing\Route;
 use Moment\Moment;
 
-if (User::canEditAsEditor()) {
-    RouterManager::instance()->before('GET', '/.*', function () {
-        $uri = RouterManager::instance()->getCurrentUri();
+//    RouterManager::instance()->before('GET', '/.*', function () {
+//        $uri = RouterManager::instance()->getCurrentUri();
+//
+//        $assetMap = [
+//            '/' => 'plugins/moment/js/add_moment_admin.js',
+//            '/moment/*' => 'plugins/moment/js/add_moment_admin.js',
+//        ];
+//
+//        foreach ($assetMap as $pattern => $asset) {
+//            if (fnmatch($pattern, $uri)) {
+//                AssetManager::addJS($asset);
+//            }
+//        }
+//    });
 
-        $assetMap = [
-            '/' => 'plugins/moment/js/add_moment_admin.js',
-            '/moment/*' => 'plugins/moment/js/add_moment_admin.js',
-        ];
-
-        foreach ($assetMap as $pattern => $asset) {
-            if (fnmatch($pattern, $uri)) {
-                AssetManager::addJS($asset);
-            }
-        }
-    });
-}
-
-RouterManager::instance()->mount('/moment', function () {
-    RouterManager::instance()->get('/([a-z0-9_-]+)', '\Moment\MomentTemplate@page');
-    RouterManager::instance()->match('GET|POST', '/insert/(\d+)(/\w+)?(/[a-z0-9_-]+)?', '\Moment\MomentController@insert');
-    RouterManager::instance()->post('/create', '\Moment\MomentController@create');
-    RouterManager::instance()->post('/save/(\d+)(/\w+)?(/[a-z0-9_-]+)?', '\Moment\MomentController@save');
-    RouterManager::instance()->post('/update/(\d+)(/\w+)?(/[a-z0-9_-]+)?', '\Moment\MomentController@update');
-    RouterManager::instance()->post('/delete/(\d+)(/\w+)?(/[a-z0-9_-]+)?', '\Moment\MomentController@delete');
+Route::mount('/moment', function () {
+    Route::get('/([a-z0-9_-]+)', '\Moment\MomentTemplate@page')
+        ->before('\Ivy\Controller\AdminController@before');
+    Route::post('/insert/(\d+)(/\w+)?(/[a-z0-9_-]+)?', '\Moment\MomentTemplate@insert')
+        ->before('\Ivy\Controller\AdminController@before');
+    Route::post('/create', '\Moment\MomentController@create')
+        ->before('\Ivy\Controller\AdminController@before');
+    Route::post('/save/(\d+)(/\w+)?(/[a-z0-9_-]+)?', '\Moment\MomentController@save')
+        ->before('\Ivy\Controller\AdminController@before');
+    Route::post('/update/(\d+)(/\w+)?(/[a-z0-9_-]+)?', '\Moment\MomentController@update')
+        ->before('\Ivy\Controller\AdminController@before');
+    Route::post('/delete/(\d+)(/\w+)?(/[a-z0-9_-]+)?', '\Moment\MomentController@delete')
+        ->before('\Ivy\Controller\AdminController@before');
 });
 
 ItemRegistry::register('moment', Moment::class);
