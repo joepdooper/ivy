775f392dd36350aaa3d2ee51c3d7bacc21736ae4
Joep <joepdooper@hotmail.com>
2026-04-28 22:47:35 +0200

    Routes in template

--- templates/base/template.php
+++ templates/base/template.php
@@ -1,7 +1,13 @@
 <?php
 
 use Ivy\Manager\AssetManager;
+use Ivy\Manager\TemplateManager;
 
 AssetManager::addCSS('css/style.css');
 AssetManager::addCSS('css/custom.css');
 AssetManager::addJS('js/twinspark.min.js');
+
+require TemplateManager::file('routes/web.php');
+require TemplateManager::file('routes/user.php');
+require TemplateManager::file('routes/admin.php');
+require TemplateManager::file('routes/error.php');
