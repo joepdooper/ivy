78a9ca702de6990ff1052456d01dec967a666277
Joep <joepdooper@hotmail.com>
2026-04-30 00:20:12 +0200

    Settings handle

--- templates/base/template.php
+++ templates/base/template.php
@@ -1,7 +1,10 @@
 <?php
 
+use Ivy\Handler\MinifyCssHandler;
+use Ivy\Handler\MinifyJsHandler;
 use Ivy\Manager\AssetManager;
 use Ivy\Manager\TemplateManager;
+use Ivy\Registry\SettingRegistry;
 
 AssetManager::addCSS('css/style.css');
 AssetManager::addCSS('css/custom.css');
@@ -11,3 +14,11 @@ TemplateManager::require('routes/web.php');
 TemplateManager::require('routes/user.php');
 TemplateManager::require('routes/admin.php');
 TemplateManager::require('routes/error.php');
+
+SettingRegistry::define('Minify CSS', [
+    'handler' => MinifyCssHandler::class,
+]);
+
+SettingRegistry::define('Minify JS', [
+    'handler' => MinifyJsHandler::class,
+]);
