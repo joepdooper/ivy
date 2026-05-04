ac9610fcc55b17e397cca300db92c7e04f176191
Joep <joepdooper@hotmail.com>
2026-05-04 09:16:38 +0200

    Update form

--- language/en/form.php
+++ language/en/form.php
@@ -2,10 +2,10 @@
 
 return [
     'rules' => [
-        'username' => ':field must be 3–20 characters long, start with a letter, and may only contain letters, numbers, dots, underscores, or dashes.',
-        'password' => ':field must be 16+ characters, incl. 1 uppercase, 1 number, 1 special character.',
-        'name' => 'The field :field contains invalid characters.',
-        'unique' => 'The field :field must be unique.',
-        'image' => 'The field :field must contain an image.'
+        'username' => 'The :field field must be 3–20 characters long, start with a letter, and may only contain letters, numbers, dots, underscores, or dashes.',
+        'password' => 'The :field field must be 12+ characters, incl. 1 uppercase, 1 number, 1 special character.',
+        'name' => 'The :field field contains invalid characters.',
+        'unique' => 'The :field field must be unique.',
+        'image' => 'The :field field must contain an image.'
         ]
 ];
