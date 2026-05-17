63d35b6feb11cd8718cb4928d77bda40523feeff
Joep <joepdooper@hotmail.com>
2026-05-18 01:52:09 +0200

    Update with new core

--- plugins/nextcloudapi/classes/NextcloudApiInterface.php
+++ plugins/nextcloudapi/classes/NextcloudApiInterface.php
@@ -4,6 +4,7 @@ namespace NextcloudApi;
 
 use Ivy\Plugin\Application\Contracts\PluginInterface;
 use Ivy\Shared\Presentation\Routing\Route;
+use Ivy\User\Domain\Entity\Profile;
 use Ivy\User\Domain\Entity\User;
 
 class NextcloudApiInterface implements PluginInterface
@@ -17,7 +18,17 @@ class NextcloudApiInterface implements PluginInterface
                 ->before('\Ivy\User\Presentation\Controller\AdminController@before');
         });
 
-        User::observe(UserObserver::class);
+        Profile::created(function (Profile $user) {
+            d('Created profile: ' . $user->id);die;
+        });
+
+        Profile::saved(function (Profile $user) {
+            d('Saved user: ' . $user->id);die;
+        });
+
+        Profile::deleted(function (Profile $user) {
+            d('Deleted user: ' . $user->id);die;
+        });
     }
 
     public function install(): void
