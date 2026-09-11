ed07165ccbe7a91aa07cee14bb24ef1fde0fcdeb
joep <joepdooper@hotmail.com>
2026-09-12 00:14:04 +0200

    Update

--- docker/mysql/ivy.sql
+++ docker/mysql/ivy.sql
@@ -203,9 +203,9 @@ INSERT INTO `settings` (`id`, `name`, `bool`, `value`, `info`, `token`, `plugin_
 (3, 'Minify JS', 0, NULL, 'JS minifier', NULL, NULL, 1, NULL, NULL),
 (4, 'Registration role', 0, 'EDITOR', 'After registration set user role', NULL, NULL, 1, NULL, NULL);
 
-INSERT INTO `templates` (`id`, `package`, `type`, `created_at`, `updated_at`) VALUES
-(1, 'cluys/ivy-base-template', 'base', NULL, NULL),
-(2, 'cluys/ivy-demo-template', 'sub', NULL, NULL);
+INSERT INTO `templates` (`id`, `package`, `type`, `url`, `created_at`, `updated_at`) VALUES
+(1, 'cluys/ivy-base-template', 'base', 'base', NULL, NULL),
+(2, 'cluys/ivy-demo-template', 'sub', 'DEMO',NULL, NULL);
 
 INSERT INTO `users` (`id`, `email`, `password`, `username`, `status`, `verified`, `resettable`, `roles_mask`, `registered`, `last_login`, `force_logout`) VALUES
 (1, 'admin@localhost.test', '$2y$12$ILvFcrz1kxPnAlwr6fFa4OWTW7EeDK5Uf1KFgdSFCzkXpxL5miV5C', 'ivy', 0, 1, 1, 263169, 1701517536, 1777807753, 0);
